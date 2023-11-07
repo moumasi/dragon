c557435fe9b20758021715d60a80fea30f0bc82d
Nazmin <tom@ex.com>
2023-11-07 02:47:08 -0500

    dragon

---
+++ src/play/movie.java
@@ -0,0 +1,30 @@
+package play;
+
+public class movie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		movie.dabang();
+
+	}
+	public static void dabang() {
+		String actorname="Salman khan";
+		System.out.println(actorname);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
